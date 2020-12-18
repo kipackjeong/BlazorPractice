71b62ab3e34e55d72c9054730ddaef0afda217da
kipack <kipack.jeong@outlook.com>
2020-12-18 17:10:04 -0500

    Added Person Page Added Image input Added Markdown component

---
+++ BlazorPracticeServer.Entity/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorPracticeServer.Entity
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
