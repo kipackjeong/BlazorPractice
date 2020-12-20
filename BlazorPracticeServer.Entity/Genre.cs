4b92fbed748a5d44f7228fc2727dea56a9fc7c40
kipack <kipack.jeong@outlook.com>
2020-12-19 20:16:19 -0500

    Added Genre Multiselector

--- BlazorPracticeServer.Entity/Genre.cs
+++ BlazorPracticeServer.Entity/Genre.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorPracticeServer.Entity
 {
@@ -10,5 +11,7 @@ namespace BlazorPracticeServer.Entity
         [Required(ErrorMessage = "This field is required")]
         public string Name { get; set; }
         public ICollection<MovieGenre> MovieGenres { get; set; }
+
+        
     }
 }
--- BlazorPracticeServer.Entity/Movie.cs
+++ BlazorPracticeServer.Entity/Movie.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorPracticeServer.Entity
 {
-    public partial class Movie
+    public class Movie
     {
         [Key] public int Id { get; set; }
         [Required] public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Poster { get; set; }
+        [Required] public string Summary { get; set; }
+        [Required] public DateTime ReleaseDate { get; set; }
+        [Required] public string Poster { get; set; }
+        [Required] public bool InTheater { get; set; }
+        [Required] public string Trailer { get; set; }
 
         public ICollection<MovieGenre> MovieGenres { get; set; }
+
+        [NotMapped] public Genre Genre { get; set; }
     }
 }
