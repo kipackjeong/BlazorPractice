0c94e2be1aba78963e085983ccf06c41190a87e2
kipack <kipack.jeong@outlook.com>
2020-12-21 18:39:23 -0500

    Added Genre APi controller

--- BlazorPracticeServer/Services/MovieService.cs
+++ BlazorPracticeServer/Services/MovieService.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorPracticeServer.Brokers.Api;
+using BlazorPracticeServer.Entity;
 
 namespace BlazorPracticeServer.Services
 {
     public class MovieService : IMovieService
     {
         //TODO 2: Implement MovieService
+        private readonly IApiBroker apiBroker;
 
+
+        public MovieService(
+            IApiBroker apiBroker)
+        {
+            this.apiBroker = apiBroker;
+        }
+
+        public async ValueTask<Movie> RegisterStudentAsync(Movie movie) => 
+            await this.apiBroker.PostMovieAsync(movie);
     }
 }
